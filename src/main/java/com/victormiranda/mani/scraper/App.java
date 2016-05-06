0dfdf8994bbd7cf1234d5426e7ec10477abb5dac
Victor MIranda <victormiranda@gmail.com>
2016-05-06 11:36:30 +0100

    Context can't be closed, application needs to be alive

--- src/main/java/com/victormiranda/mani/scraper/App.java
+++ src/main/java/com/victormiranda/mani/scraper/App.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 
-    private App() {}
-
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args).close();
+        SpringApplication.run(App.class, args);
     }
 }
