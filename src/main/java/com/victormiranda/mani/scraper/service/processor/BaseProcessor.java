c344e5c004d2f195e1196f980337038f44249c51
Victor MIranda <victormiranda@gmail.com>
2016-05-07 10:14:19 +0100

    money method in base processor

--- src/main/java/com/victormiranda/mani/scraper/service/processor/BaseProcessor.java
+++ src/main/java/com/victormiranda/mani/scraper/service/processor/BaseProcessor.java
@@ -1,6 +1,7 @@
 package com.victormiranda.mani.scraper.service.processor;
 
 import com.victormiranda.mani.scraper.bean.NavigationSession;
+import com.victormiranda.mani.scraper.util.Validate;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
 
 public class BaseProcessor {
@@ -34,6 +36,19 @@ public class BaseProcessor {
             LOGGER.error("Error processing url " + url, e);
         }
         return parsedDocument;
+    }
+
+    public static BigDecimal money(String text) {
+        if (!Validate.notEmpty(text)) {
+            throw new IllegalArgumentException("Incorrect value " + text);
+        }
+
+        try {
+            return new BigDecimal(text.replace("€","").replace(",",""));
+        } catch (NumberFormatException e) {
+            LOGGER.error("Error processing " + text);
+            throw e;
+        }
     }
 
     private void delay(final long i) {
